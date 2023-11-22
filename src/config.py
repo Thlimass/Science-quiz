1b1627ef2268dae854a0165db0794693d5482f4a
Thiago Lima <thlima.silva.souza@gmail.com>
2023-11-21 21:56:20 -0300

    add config file

---
+++ src/config.py
@@ -0,0 +1,15 @@
+
+
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://admin:admin@localhost:5432/postgres'
+
+
+config = {
+    "development": DevelopmentConfig,
+}
