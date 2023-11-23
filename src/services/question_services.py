7130b93288a2fbd157a50744f209e92953acf056
Thiago Lima <thlima.silva.souza@gmail.com>
2023-11-22 22:44:30 -0300

    update question services

--- src/services/question_services.py
+++ src/services/question_services.py
@@ -1,20 +1,23 @@
 from flask import jsonify, request
 import uuid
 from .. import db
+from ..entities.category_entity import Category
 from ..entities.question_entity import Question
 
 
-def create():
+def create(category_name):
     request_form = request.json
-    print(request_form)
     question_id = str(uuid.uuid4())
-    question = Question(
-        id=question_id,
-        category_id="7487fa5a-4796-4c71-a0f1-7d18031fa3e9",
-        question_text=request_form['question_text'],
-        correct_answer=request_form['correct_answer'],
-        incorrect_answer=request_form['incorrect_answer']
-    )
+    category = Category.query.filter_by(category_name=category_name).first()
+
+    if category:
+        question = Question(
+            id=question_id,
+            category_id=category.id,
+            question_text=request_form['question_text'],
+            correct_answer=request_form['correct_answer'],
+            incorrect_answer=request_form['incorrect_answer']
+        )
 
     db.session.add(question)
     db.session.commit()
