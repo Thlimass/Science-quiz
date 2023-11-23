5bd4e55e289596c0f9ff4c9d32d3bb037531b144
Thiago Lima <thlima.silva.souza@gmail.com>
2023-11-22 22:27:48 -0300

    add question entity

---
+++ src/entities/question_entity.py
@@ -0,0 +1,20 @@
+from sqlalchemy import ForeignKey, inspect, ARRAY, String
+from .category_entity import Category
+from datetime import datetime
+from .. import db
+
+
+class Question(db.Model):
+    id = db.Column(db.String(100), primary_key=True, nullable=False, unique=True)
+    category_id = db.Column(ForeignKey(Category.id))
+    question_text = db.Column(db.String(9999))
+    correct_answer = db.Column(db.String(1000))
+    incorrect_answer = db.Column(ARRAY(String))
+    created_at = db.Column(db.DateTime(timezone=True), default=datetime.now)
+    updated_at = db.Column(db.DateTime(timezone=True), default=datetime.now, onupdate=datetime.now)
+
+    def toDict(self):
+        return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
+
+    def __repr__(self):
+        return "<%r>" % self.question_id
