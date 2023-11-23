124a74e1a347945336951010ca0dc35cfb2e1ab1
Thiago Lima <thlima.silva.souza@gmail.com>
2023-11-22 22:44:42 -0300

    update question controller

--- src/controllers/question_controller.py
+++ src/controllers/question_controller.py
@@ -4,9 +4,9 @@ from ..app import app
 from ..services.question_services import create
 
 
-@app.route("/question", methods=['POST'])
-def list_create_questions():
+@app.route("/question/<category_name>", methods=['POST'])
+def list_create_questions(category_name):
     if request.method == 'POST':
-        return create()
+        return create(category_name)
     else:
         return 'Method is Not Allowed'
