115407e5e7924eb598d64d2c9c0d300330d64abd
Thiago Lima <thlima.silva.souza@gmail.com>
2023-11-25 01:41:01 -0300

    update category selection screen

--- src/view/category_selection_screen.py
+++ src/view/category_selection_screen.py
@@ -1,15 +1,24 @@
 import pygame
+import requests
 
 
-def show_category_selection_screen(categories):
+def show_category_selection_screen():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption('Selecione uma Categoria')
+    pygame.display.set_caption('Exibindo Lista')
 
-    font = pygame.font.Font(None, 36)
+    font = pygame.font.Font(None, 24)
+    white = (255, 255, 255)
+    black = (0, 0, 0)
 
-    category_y = 50
-    button_height = 40
+    # Lista de itens a serem exibidos
+    items = ["Item 1", "Item 2", "Item 3", "Item 4"]
+
+    response = requests.get('http://127.0.0.1:5000/category')
+    print(response.content)
+    if response.status_code == 200:
+        categories_list = response.content
+    categories = [item['category_name'] for item in eval(categories_list.decode())]
 
     running = True
     while running:
@@ -17,19 +26,28 @@ def show_category_selection_screen(categories):
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.fill((255, 255, 255))  # Fundo branco
+        screen.fill(white)
+
+        # Define o tamanho e espaçamento dos retângulos
+        rect_width, rect_height = 200, 30
+        vertical_spacing = 10
+
+        # Calcula a posição inicial para centralizar na tela
+        total_height = len(categories) * (rect_height + vertical_spacing)
+        start_y = (screen.get_height() - total_height) // 2
 
         for category in categories:
-            # Crie um retângulo para o botão da categoria
-            button_rect = pygame.Rect(50, category_y, 200, button_height)
-            pygame.draw.rect(screen, (200, 200, 200), button_rect)  # Desenhe o botão
+            # Cria um retângulo para cada item
+            rect = pygame.Rect((screen.get_width() - rect_width) // 2, start_y, rect_width, rect_height)
+            pygame.draw.rect(screen, black, rect)
 
-            # Renderize o texto da categoria no botão
-            text = font.render(category['name'], True, (0, 0, 0))
-            text_rect = text.get_rect(center=button_rect.center)
+            # Renderiza o texto do item
+            text = font.render(category, True, white)
+            text_rect = text.get_rect(center=rect.center)
+
             screen.blit(text, text_rect)
 
-            category_y += button_height + 10  # Adicione um espaço entre os botões
+            start_y += rect_height + vertical_spacing  # Aumenta a posição Y para o próximo item
 
         pygame.display.flip()
 
