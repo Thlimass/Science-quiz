69c96773b4309d707294ea9c8fbd6bf070332919
Thiago Lima <thlima.silva.souza@gmail.com>
2023-11-25 01:40:25 -0300

    update main screen

--- src/view/main_screen.py
+++ src/view/main_screen.py
@@ -2,7 +2,6 @@ import pygame
 from flask import Flask
 
 from src.view.category_selection_screen import show_category_selection_screen
-from ..services.category_services import list_all_categories
 
 app = Flask(__name__)
 
@@ -19,10 +18,11 @@ def show_main_screen():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                categories = list_all_categories()
-                print(categories)
-                show_category_selection_screen(categories)
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                mouse_pos = pygame.mouse.get_pos()
+                button_rect = pygame.Rect(300, 300, 200, 50)
+                if button_rect.collidepoint(mouse_pos):
+                    show_category_selection_screen()  # Passe as categorias para a função
 
         screen.fill((255, 255, 255))  # Fundo branco
 
@@ -30,11 +30,21 @@ def show_main_screen():
         text_rect = text.get_rect(center=(400, 200))
         screen.blit(text, text_rect)
 
-        pygame.draw.rect(screen, (0, 128, 255), (300, 300, 200, 50))  # Botão
+        button_rect = pygame.Rect(300, 300, 200, 50)  # Retângulo do botão
+        pygame.draw.rect(screen, (0, 128, 255), button_rect)  # Botão
         button_text = font.render('Start', True, (255, 255, 255))
-        button_text_rect = button_text.get_rect(center=(400, 325))
+        button_text_rect = button_text.get_rect(center=button_rect.center)
         screen.blit(button_text, button_text_rect)
+
+        # Verifica se o cursor está sobre o botão
+        mouse_pos = pygame.mouse.get_pos()
+        if button_rect.collidepoint(mouse_pos):
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+        else:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 
         pygame.display.flip()
 
     pygame.quit()
+
+    show_category_selection_screen()
